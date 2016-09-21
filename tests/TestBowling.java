cb4049c5ef38b47c8f40bd4ccb67fe589c8af74e
somename <someemail>
2016-09-21 14:50:08 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -3,30 +3,32 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestBowling {
+	
+	////////////////
+	//FRAME CREATION
 
 	@Test(expected=BowlingException.class)
 	public void test_NegativeScore1() throws BowlingException {
-		BowlingGame game = new BowlingGame();
-		game.addFrame(new Frame(-1, 2));
+		Frame frame = new Frame(-1, 2);
 	}
 
 	@Test(expected=BowlingException.class)
 	public void test_NegativeScore2() throws BowlingException {
-		BowlingGame game = new BowlingGame();
-		game.addFrame(new Frame(3, -2));
+		Frame frame = new Frame(3, -2);
 	}
 	
 	@Test(expected=BowlingException.class)
 	public void test_TooBigThrow() throws BowlingException {
-		BowlingGame game = new BowlingGame();
-		game.addFrame(new Frame(13, 2));
+		Frame frame = new Frame(13, 2);
 	}
 	
 	@Test(expected=BowlingException.class)
 	public void test_TooBigScore() throws BowlingException {
-		BowlingGame game = new BowlingGame();
-		game.addFrame(new Frame(9, 2));
+		Frame frame = new Frame(9, 2);
 	}
+	
+	//////////////
+	//BASIC SCORES
 	
 	@Test
 	public void test_FrameScore() throws BowlingException {
