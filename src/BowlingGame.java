6e00dc74b93a4c5cb7ead38f2c059a26acb9dbfe
somename <someemail>
2016-09-21 15:23:40 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -30,6 +30,9 @@ public class BowlingGame {
 	
 	// Returns the game score
 	public int score() {
+		List<Frame> fullScores = new ArrayList<Frame>();
+		fullScores.addAll(frames);
+		
 		int score = 0;
 		int frameScore = 0;
 		
